a537b7ff3574d76c01ed4a21237c7e7c5de0964f
David Salek <salekd@gmail.com>
2017-07-24 21:22:14 +0000

    correct file name

--- upload_file.py
+++ upload_file.py
@@ -18,12 +18,12 @@ filename_local = max(full_paths, key=os.path.getctime)
 # The last two digits stand for the frame number.
 # http://htmlpreview.github.io/?https://github.com/Motion-Project/motion/blob/master/motion_guide.html#picture_filename
 # http://htmlpreview.github.io/?https://github.com/Motion-Project/motion/blob/master/motion_guide.html#conversion_specifiers
-filename_s3 = os.path.basename(filename_local)[2:]
+filename_s3 = os.path.basename(filename_local)[3:]
 bucket_name = 'rpizero-smart-camera-upload'
 
 # Uploads the given file using a managed uploader, which will split up large
 # files automatically and upload parts in parallel.
-print("Uploading file {} to Amazon S3".format(filename_local))
+print("Uploading file {} to Amazon S3".format(filename_s3))
 s3.upload_file(filename_local, bucket_name, filename_s3)
 
 # Remove the image from the local file system
